285f7d82db13e742f37b3dc3bbeebc9afafc4f43
oazambranoa <oazambranoa@unal.edu.co>
2023-11-29 10:19:16 -0500

    Creacion y terminación del ejercicio

---
+++ src/main/java/com/mycompany/friends/Friends.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.friends;
+
+/**
+ *
+ * @author omarz
+ */
+public class Friends {
+
+    public static void main(String[] args) {
+        Formulario frm = new Formulario();
+        frm.setVisible(true);
+    }
+}
